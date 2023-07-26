ba32491b9c1c7a822946dd6470b4221ae9fe6fd6
Plenzel <zirn.johannes@gmail.com>
2023-07-26 16:45:56 +0200

    added questions

---
+++ js/script.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist der Zweck der HTML-Sprache?",
+        "answer_1": "Grafikdesign",
+        "answer_2": "Audio-Streaming",
+        "answer_3": "Webentwicklung",
+        "answer_4": "Filmproduktion",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um eine Überschrift erster Ordnung (H1) zu erstellen?",
+        "answer_1": "<h1>",
+        "answer_2": "<head>",
+        "answer_3": "<p>",
+        "answer_4": "<br>",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist die Bedeutung der HTML-Tag-Attribute 'href'?",
+        "answer_1": "Es definiert den Namen des Elements.",
+        "answer_2": "Es verweist auf eine externe Stylesheet-Datei.",
+        "answer_3": "Es gibt den Wert einer Zelle in einer Tabelle an.",
+        "answer_4": "Es gibt den Hyperlink für einen Anker (Link) an.",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche HTML-Auszeichnung wird verwendet, um ein Bild einzufügen?",
+        "answer_1": "<img>",
+        "answer_2": "<picture>",
+        "answer_3": "<image>",
+        "answer_4": "<src>",
+        "right_answer": 1
+    },
+    {
+        "question": "Welches HTML-Element wird verwendet, um eine geordnete Liste zu erstellen?",
+        "answer_1": "<ul>",
+        "answer_2": "<ol>",
+        "answer_3": "<li>",
+        "answer_4": "<dl>",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist die richtige HTML-Struktur für ein einfaches HTML-Dokument?",
+        "answer_1": "<header>, <body>, <footer>",
+        "answer_2": "<head>, <main>, <footer>",
+        "answer_3": "<title>, <content>, <end>",
+        "answer_4": "<doctype>, <html>, <body>",
+        "right_answer": 4
+    }
+]
+
+function init(){
+    
+}
